c55792138f5d2c88b55dbba65038211a3daf01a5
Christopher Casolin <chriscasolin@Christophers-MacBook-Air-5.local>
2023-07-25 15:32:43 +1000

    battle item default use method

--- src/main/java/dungeonmania/entities/buildables/Shield.java
+++ src/main/java/dungeonmania/entities/buildables/Shield.java
@@ -14,64 +14,69 @@ import dungeonmania.entities.inventory.Inventory;
 import dungeonmania.entities.inventory.InventoryItem;
 
 public class Shield extends Buildable {
-    public static final List<Recipe> RECIPES = buildRecipes();
-    private int durability;
-    private double defence;
+  public static final List<Recipe> RECIPES = buildRecipes();
+  private int durability;
+  private double defence;
 
-    public Shield(int durability, double defence) {
-        super(null);
-        this.durability = durability;
-        this.defence = defence;
+  public Shield(int durability, double defence) {
+    super(null);
+    this.durability = durability;
+    this.defence = defence;
+  }
+
+  @Override
+  public void use(Game game) {
+    durability--;
+    if (durability <= 0) {
+      game.getPlayer().remove(this);
     }
+  }
 
-    @Override
-    public void use(Game game) {
-        durability--;
-        if (durability <= 0) {
-            game.getPlayer().remove(this);
-        }
+  @Override
+  public BattleStatistics applyBuff(BattleStatistics origin) {
+    return BattleStatistics.applyBuff(origin, new BattleStatistics(0, 0, defence, 1, 1));
+  }
+
+  @Override
+  public int getDurability() {
+    return durability;
+  }
+
+  @Override
+  public void setDurability(int durability) {
+    this.durability = durability;
+  }
+
+  @Override
+  public List<Recipe> getRecipes() {
+    return RECIPES;
+  }
+
+  private static List<Recipe> buildRecipes() {
+    // This implementation would word better if recipes were saved in the config
+    // file and could be read into the list. Since I don't think we can touch that
+    // file, building each recipe like this still works.
+    List<Recipe> recipes = new ArrayList<>();
+
+    Map<Class<? extends InventoryItem>, Integer> recipe1 = new HashMap<>();
+    recipe1.put(Wood.class, 2);
+    recipe1.put(Treasure.class, 1);
+    recipes.add(new Recipe(recipe1));
+
+    Map<Class<? extends InventoryItem>, Integer> recipe2 = new HashMap<>();
+    recipe2.put(Wood.class, 2);
+    recipe2.put(Key.class, 1);
+    recipes.add(new Recipe(recipe2));
+
+    return recipes;
+  }
+
+  public static boolean canBuild(Inventory inventory) {
+    for (Recipe recipe : RECIPES) {
+      if (recipe.canBuild(inventory)) {
+        return true;
+      }
     }
-
-    @Override
-    public BattleStatistics applyBuff(BattleStatistics origin) {
-        return BattleStatistics.applyBuff(origin, new BattleStatistics(0, 0, defence, 1, 1));
-    }
-
-    @Override
-    public int getDurability() {
-        return durability;
-    }
-
-    @Override
-    public List<Recipe> getRecipes() {
-        return RECIPES;
-    }
-
-    private static List<Recipe> buildRecipes() {
-        // This implementation would word better if recipes were saved in the config
-        // file and could be read into the list. Since I don't think we can touch that
-        // file, building each recipe like this still works.
-        List<Recipe> recipes = new ArrayList<>();
-
-        Map<Class<? extends InventoryItem>, Integer> recipe1 = new HashMap<>();
-        recipe1.put(Wood.class, 2);
-        recipe1.put(Treasure.class, 1);
-        recipes.add(new Recipe(recipe1));
-
-        Map<Class<? extends InventoryItem>, Integer> recipe2 = new HashMap<>();
-        recipe2.put(Wood.class, 2);
-        recipe2.put(Key.class, 1);
-        recipes.add(new Recipe(recipe2));
-
-        return recipes;
-    }
-
-    public static boolean canBuild(Inventory inventory) {
-        for (Recipe recipe : RECIPES) {
-            if (recipe.canBuild(inventory)) {
-                return true;
-            }
-        }
-        return false;
-    }
+    return false;
+  }
 }
