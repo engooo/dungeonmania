86bc5ca839b15c1cbaa52d49ef27ba9a19d43e69
Christopher Casolin <chriscasolin@Christophers-MacBook-Air-5.local>
2023-07-27 01:31:32 +1000

    sceptre tests passing - but one shouldnt

--- src/main/java/dungeonmania/entities/collectables/SunStone.java
+++ src/main/java/dungeonmania/entities/collectables/SunStone.java
@@ -2,7 +2,9 @@ package dungeonmania.entities.collectables;
 
 import dungeonmania.util.Position;
 
-public class SunStone extends Treasure {
+public class SunStone extends CollectableEntity {
+  // This class needs to extend treasure, but that stuffs up the recipe.
+  // Future problem, find a way around this. Probably inheritance?
   public SunStone(Position position) {
     super(position);
   }
--- src/test/java/dungeonmania/task2/SceptreTest.java
+++ src/test/java/dungeonmania/task2/SceptreTest.java
@@ -2,11 +2,13 @@ package dungeonmania.task2;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import dungeonmania.DungeonManiaController;
+import dungeonmania.exceptions.InvalidActionException;
 import dungeonmania.mvp.TestUtils;
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.util.Direction;
@@ -84,10 +86,12 @@ public class SceptreTest {
     assertEquals(4, TestUtils.getInventory(res, "sceptre").size());
     res = assertDoesNotThrow(() -> dmc.build("sceptre"));
     assertEquals(5, TestUtils.getInventory(res, "sceptre").size());
+    // Check sun_stone remains
+    assertEquals(1, TestUtils.getInventory(res, "sun_stone").size());
 
-    // Case6: Pick up 1 wood, 2 sun_stones
+    // Case6: Pick up 1 wood, 1 more sun_stone
     res = dmc.tick(Direction.DOWN);
-    res = dmc.tick(Direction.LEFT);
+    res = dmc.tick(Direction.LEFT); // sun_stone at this tile removed from dungeon
     res = dmc.tick(Direction.LEFT);
     assertEquals(1, TestUtils.getInventory(res, "wood").size());
     assertEquals(2, TestUtils.getInventory(res, "sun_stone").size());
@@ -96,18 +100,18 @@ public class SceptreTest {
     assertEquals(5, TestUtils.getInventory(res, "sceptre").size());
     res = assertDoesNotThrow(() -> dmc.build("sceptre"));
     assertEquals(6, TestUtils.getInventory(res, "sceptre").size());
+    // Check sun_stone remains
+    assertEquals(1, TestUtils.getInventory(res, "sun_stone").size());
 
     // Case7: uncraftable: Pick up 1 sun_stone, 1 wood (test they uncraftable)
-    res = dmc.tick(Direction.DOWN);
+    res = dmc.tick(Direction.DOWN); // sun_stone at this tile removed from dungeon
     res = dmc.tick(Direction.RIGHT);
     assertEquals(1, TestUtils.getInventory(res, "wood").size());
     assertEquals(1, TestUtils.getInventory(res, "sun_stone").size());
 
-    // Build Sceptre
+    // Try to build Sceptre
     assertEquals(6, TestUtils.getInventory(res, "sceptre").size());
-    res = assertDoesNotThrow(() -> dmc.build("sceptre"));
-    assertEquals(6, TestUtils.getInventory(res, "sceptre").size());
-
+    assertThrows(InvalidActionException.class, () -> dmc.build("sceptre"));
   }
 
   @Test
